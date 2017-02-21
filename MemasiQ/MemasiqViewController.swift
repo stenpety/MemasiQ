7c6c41b44217ab5d08f8e5dd8564555d3cac7995
Petr Stenin <stenpety@petrs-mbp.wimaxion>
2017-02-21 21:42:13 +0400

    Notifications (default for keyboard) and keyboard appear/hide view move implemented. Custom notifications created (not used). Hide keyboard on view transition (screen rotation) implemented

--- MemasiQ/MemasiqViewController.swift
+++ MemasiQ/MemasiqViewController.swift
@@ -17,6 +17,7 @@ class MemasiqViewController: UIViewController {
     @IBOutlet weak var cameraBarButton: UIBarButtonItem!
     
     let textFieldDelegate = TextFieldDelegate()
+    let bottomTextFieldDelegate = BottomTextFieldDelegate()
     
     let memasTextAttributes:[String:Any] = [
         NSStrokeColorAttributeName: UIColor.black,
@@ -49,13 +50,44 @@ class MemasiqViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        self.subscribeToKeyboardNotifications()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
+        self.unsubscribeFromKeyboardNotifications()
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        bottomTextField.resignFirstResponder()
+    }
     
+    // Move the view when keyboard appears (while editing bottom text)
+    func keyboardWillShow(notification: NSNotification) {
+        view.frame.origin.y -= getKeyboardHeight(notification: notification)
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        view.frame.origin.y += getKeyboardHeight(notification: notification)
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    // Subscribe/unsubscribe to notifications
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
     
 }
 
---
+++ MemasiQ/NSNotificationCenterKeys.swift
@@ -0,0 +1,15 @@
+//
+//  NSNotificationCenterKeys.swift
+//  MemasiQ
+//
+//  Created by Petr Stenin on 21/02/2017.
+//  Copyright © 2017 Petr Stenin. All rights reserved.
+//
+
+import Foundation
+
+// Notification key for showing keyboard
+let showKeyboardNotificationKey = NSNotification.Name("com.stenpety.showKeyboardNotificationKey")
+
+// Notification key for hiding keyboard
+let hideKeyboardNotificationKey = NSNotification.Name("com.stenpety.hideKeyboardNotificationKey")
