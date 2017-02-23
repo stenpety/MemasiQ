9331b1d5350ec581cb1bd78943b356f7e35d88ac
Petr Stenin <stenpety@petrs-mbp.wimaxion>
2017-02-23 22:09:31 +0400

    ImagePicker controller implemented. 'Camera' button deactivated if camera is not available.

--- MemasiQ/MemasiqViewController.swift
+++ MemasiQ/MemasiqViewController.swift
@@ -8,14 +8,15 @@
 
 import UIKit
 
-class MemasiqViewController: UIViewController {
+class MemasiqViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    // Define outlets
+    // MARK: Outlets
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var memasImageView: UIImageView!
     @IBOutlet weak var cameraBarButton: UIBarButtonItem!
     
+    // MARK: Properties
     let textFieldDelegate = TextFieldDelegate()
     
     let memasTextAttributes:[String:Any] = [
@@ -27,12 +28,12 @@ class MemasiqViewController: UIViewController {
     let TOP_PLACEHOLDER = "TOP"
     let BOTTOM_PLACEHOLDER = "BOTTOM"
     
-    
+    // MARK: View setup
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Setup ImageView to properly scale selected image
-        memasImageView.contentMode = UIViewContentMode.center
+        memasImageView.contentMode = UIViewContentMode.scaleAspectFit
         
         // Setup Top text field
         topTextField.defaultTextAttributes = memasTextAttributes
@@ -50,6 +51,7 @@ class MemasiqViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.subscribeToKeyboardNotifications()
+        cameraBarButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -62,7 +64,7 @@ class MemasiqViewController: UIViewController {
         bottomTextField.resignFirstResponder()
     }
     
-    // Move the view when keyboard appears (while editing bottom text)
+    // MARK: Move the view when keyboard appears (while editing bottom text)
     func keyboardWillShow(notification: NSNotification) {
         if bottomTextField.isFirstResponder {
             view.frame.origin.y -= getKeyboardHeight(notification: notification)
@@ -81,7 +83,7 @@ class MemasiqViewController: UIViewController {
         return keyboardSize.cgRectValue.height
     }
     
-    // Subscribe/unsubscribe to notifications
+    // MARK: Subscribe/unsubscribe to notifications
     func subscribeToKeyboardNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: .UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: .UIKeyboardWillHide, object: nil)
@@ -90,6 +92,33 @@ class MemasiqViewController: UIViewController {
     func unsubscribeFromKeyboardNotifications() {
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
         NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    // MARK: Image Picker Controller methods
+    @IBAction func pickAnImage(_ sender: UIBarButtonItem) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        // 'Camera' buttom tag is 3, 'PhotoLib' tag is 4
+        switch sender.tag {
+        case 3:
+            pickerController.sourceType = .camera
+        case 4:
+            pickerController.sourceType = .photoLibrary
+        default:
+            break
+        }
+        self.present(pickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            memasImageView.image = pickedImage
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
     }
     
 }
